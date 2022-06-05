13e26f75b843dd1a8979c687de752f890c4f4227
kzhein <kyawzinhein6@gmail.com>
2022-06-05 20:50:02 +0630

    Error handling

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filters/all-exceptions-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const errorMessages = [];
+        errors.forEach((error) => {
+          Object.keys(error.constraints).forEach((constraint) => {
+            errorMessages.push(error.constraints[constraint]);
+          });
+        });
+
+        return new BadRequestException(errorMessages[0]);
+      },
+    }),
+  );
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
+
   await app.listen(3000);
 }
 bootstrap();
